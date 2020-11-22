4d1f0936d56f4d09298747660d89140eb72e45da
fgnm <fgnm.dev@gmail.com>
2020-11-22 14:21:27 +0100

    Add MenuAPI

---
+++ src/main/java/games/rednblack/h2d/common/MenuAPI.java
@@ -0,0 +1,12 @@
+package games.rednblack.h2d.common;
+
+public class MenuAPI {
+    private static final String HyperLap2DMenuBar = "games.rednblack.editor.view.HyperLap2DMenuBar";
+
+    public static final String EDIT_MENU = HyperLap2DMenuBar + ".EDIT_MENU";
+    public static final String FILE_MENU = HyperLap2DMenuBar + ".FILE_MENU";
+    public static final String WINDOW_MENU = HyperLap2DMenuBar + ".WINDOW_MENU";
+    public static final String HELP_MENU = HyperLap2DMenuBar + ".HELP_MENU";
+
+    public static final String RESOURCE_MENU = HyperLap2DMenuBar + ".RESOURCE_MENU";
+}
