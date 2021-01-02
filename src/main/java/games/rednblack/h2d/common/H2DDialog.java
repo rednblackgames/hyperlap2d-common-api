c7fd98abb9358757148d5ceff8bfac7237211627
fgnm <fgnm.dev@gmail.com>
2021-01-02 11:40:05 +0100

    Improve `H2DDialog` show/hide animation

--- src/main/java/games/rednblack/h2d/common/H2DDialog.java
+++ src/main/java/games/rednblack/h2d/common/H2DDialog.java
@@ -18,9 +18,12 @@
 
 package games.rednblack.h2d.common;
 
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -29,17 +32,36 @@ import com.kotcrab.vis.ui.widget.VisDialog;
 import com.kotcrab.vis.ui.widget.VisImageButton;
 
 public class H2DDialog extends VisDialog {
-    protected final Skin skin;
+    protected Skin skin;
+
+	static public final Interpolation.SwingOut swingOut = new Interpolation.SwingOut(1.1f);
 
     public H2DDialog(String title) {
         super(title);
-        skin = VisUI.getSkin();
-        padTop(32);
+		init();
     }
+
+    public H2DDialog(String title, String style) {
+    	super(title, style);
+    	init();
+	}
+
+    private void init() {
+		skin = VisUI.getSkin();
+		padTop(32);
+	}
 
 	@Override
 	public void hide () {
-		super.hide();
+		super.hide(Actions.sequence(
+				Actions.parallel(
+					Actions.scaleTo(0.8f, 0.8f, FADE_TIME, Interpolation.pow5Out),
+					Actions.alpha(0, FADE_TIME, Interpolation.pow3In),
+					Actions.moveBy(0, -15, FADE_TIME, Interpolation.pow5In)
+				),
+				Actions.removeListener(ignoreTouchDown, true),
+				Actions.moveBy(0, 15),
+				Actions.removeActor()));
 		onDismiss();
 	}
 
@@ -47,6 +69,23 @@ public class H2DDialog extends VisDialog {
 	public void hide (Action action) {
 		super.hide(action);
 		onDismiss();
+	}
+
+	@Override
+	public VisDialog show(Stage stage) {
+		show(stage, Actions.sequence(
+				Actions.alpha(0),
+				Actions.scaleTo(0.8f, 0.8f),
+				Actions.moveBy(0, -15),
+				Actions.parallel(
+						Actions.scaleTo(1, 1, FADE_TIME, swingOut),
+						Actions.alpha(1, FADE_TIME),
+						Actions.moveBy(0, 15, FADE_TIME)
+				)
+		));
+		setPosition(Math.round((stage.getWidth() - getWidth()) / 2), Math.round((stage.getHeight() - getHeight()) / 2));
+		setOrigin(getWidth() / 2f, getHeight() / 2f);
+		return this;
 	}
 
 	@Override
@@ -70,8 +109,7 @@ public class H2DDialog extends VisDialog {
 
 	@Override
 	public void close () {
-		super.close();
-		onDismiss();
+		hide();
 	}
 
 	protected void onDismiss() {
