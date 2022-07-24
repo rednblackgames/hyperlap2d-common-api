769f77b06e4181743fb0eeb836ab352539d18f93
fgnm <fgnm.dev@gmail.com>
2022-07-24 23:05:02 +0200

    Fix tree view auto scroll

--- src/main/java/games/rednblack/h2d/common/view/ui/StandardWidgetsFactory.java
+++ src/main/java/games/rednblack/h2d/common/view/ui/StandardWidgetsFactory.java
@@ -19,6 +19,7 @@
 package games.rednblack.h2d.common.view.ui;
 
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
@@ -242,7 +243,28 @@ public class StandardWidgetsFactory {
     }
 
     public static VisScrollPane createScrollPane(Actor actor) {
-        VisScrollPane scrollPane = new VisScrollPane(actor);
+        VisScrollPane scrollPane = new VisScrollPane(actor) {
+            //TODO remove with libGDX 1.11.1 release
+            @Override
+            public void scrollTo(float x, float y, float width, float height, boolean centerHorizontal, boolean centerVertical) {
+                validate();
+
+                float amountX = this.getScrollX();
+                if (centerHorizontal)
+                    amountX = x + (width - this.getScrollWidth()) / 2;
+                else
+                    amountX = MathUtils.clamp(amountX, x + width - this.getScrollWidth(), x);
+                scrollX(MathUtils.clamp(amountX, 0, this.getMaxX()));
+
+                float amountY = this.getScrollY();
+                y = getMaxY() - y;
+                if (centerVertical)
+                    amountY = y + (this.getScrollHeight() + height) / 2;
+                else
+                    amountY = MathUtils.clamp(amountY, y + height, y + this.getScrollHeight());
+                scrollY(MathUtils.clamp(amountY, 0, getMaxY()));
+            }
+        };
         scrollPane.addListener(new ScrollFocusListener());
         scrollPane.setFlickScroll(false);
         return scrollPane;
